2ab241e7b7f70dfd362a9a3828ae0c8b7e0f9f37
Bernard Mordan <bernardmordan@gmail.com>
2014-12-26 21:38:10 +0000

    Multi-touch achieved with 'touchmove' added to surface

--- public/js/drive.js
+++ public/js/drive.js
@@ -1,4 +1,4 @@
-var socket = io.connect('192.168.0.13:3000');
+var socket = io.connect('http://192.168.0.13');
 var Engine = famous.core.Engine;
 var Modifier = famous.core.Modifier;
 var Transform = famous.core.Transform;
@@ -11,7 +11,7 @@ var GenericSync = famous.inputs.GenericSync;
 var Transitionable = famous.transitions.Transitionable;
 GenericSync.register({
   "mouse" : MouseSync,
-  "touch" : TouchSync
+  "touch"  : TouchSync
 });
 var mainContext = Engine.createContext();
 var mainContextWidthFifth = mainContext.getSize()[0]/5;
@@ -36,21 +36,6 @@ var leftControlModifier = new Modifier({
     return Transform.translate(mainContextWidthFifth, leftYpos.get(), 0)
   }
 });
-var leftControlSync = new GenericSync({
-  "mouse" : {},
-  "touch" : {}
-});
-leftControl.pipe(leftControlSync);
-leftControlSync.on('update', function(data){
-  var delta = data.position[1]
-  leftYpos.set(delta)
-  if(delta < 0){var dir = 'forward'}else{var dir = 'reverse'}
-  socket.emit('drive', {motor: 'L', dir: dir, speed: 200});
-});
-leftControlSync.on('end', function(data){
-  leftYpos.set(0,{duration: 500, curve: Easing.outBounce })
-  socket.emit('drive', {motor: 'L', dir: 'stop', speed: 0});
-});
 var rightControl = new Surface(control);
 var rightYpos = new Transitionable(0);
 var rightControlModifier = new Modifier({
@@ -60,20 +45,26 @@ var rightControlModifier = new Modifier({
     return Transform.translate(-mainContextWidthFifth, rightYpos.get(), 0)
   }
 });
-var rightControlSync = new GenericSync({
-  "mouse" : {},
-  "touch" : {}
+leftControl.on('touchmove', function(data){
+  var delta = data.targetTouches[0].clientY-mainContextWidthFifth
+  leftYpos.set(delta)
+  if(delta < 0){var dir = 'forward'}else if(delta > 0){var dir = 'reverse'}
+  console.log(socket)
+  socket.emit('movement', {motor: 'L', dir: dir})
 });
-rightControl.pipe(rightControlSync)
-rightControlSync.on('update', function(data){
-  var delta = data.position[1]
+leftControl.on('touchend', function(data){
+  leftYpos.set(0, {duration: 633, curve: Easing.outBounce})
+  socket.emit('movement', {motor: 'L', dir: 'stop'})
+});
+rightControl.on('touchmove', function(data){
+  var delta = data.targetTouches[0].clientY-mainContextWidthFifth
   rightYpos.set(delta)
-  if(delta < 0){var dir = 'forward'}else{var dir = 'reverse'}
-  socket.emit('drive', {motor: 'R', dir: dir, speed: 200});
+  if(delta < 0){var dir = 'forward'}else if(delta > 0){var dir = 'reverse'}
+  socket.emit('movement', {motor: 'R', dir: dir})
 });
-rightControlSync.on('end', function(data){
-  rightYpos.set(0,{duration: 500, curve: Easing.outBounce });
-  socket.emit('drive', {motor: 'R', dir: 'stop', speed: 0});
+rightControl.on('touchend', function(data){
+  rightYpos.set(0, {duration: 633, curve: Easing.outBounce})
+  socket.emit('movement', {motor: 'R', dir: 'stop'})
 });
 mainContext.add(leftControlModifier).add(leftControl);
 mainContext.add(rightControlModifier).add(rightControl);
--- server.js
+++ server.js
@@ -3,29 +3,22 @@ var http = require('http').Server(server);
 var expressLayouts = require('express-ejs-layouts');
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-
 server.set('view engine', 'ejs');
 server.set('views',__dirname + '/views');
 server.use(expressLayouts);
 server.use(require('express').static(__dirname + '/public'));
-
 server.get('/', function(req, res){
   res.render('index', {layout: 'layout'})
 });
-
 server.get('/drive', function(req, res){
   res.render('drive', {layout: 'layout'});
 });
-
-http.listen(port, function(){
-  console.log('iNodebotServer running on '+port)
-});
-
+http.listen(port, function(){console.log('iNodebotServer running on '+port)});
 io.on('connection', function(connection){
   connection.on('ready', function(data){
     console.log('THREE LAWS SAFE iNodebot connected');
   });
-  connection.on('drive', function(data){
+  connection.on('movement', function(data){
     console.log(data)
   });
 });
