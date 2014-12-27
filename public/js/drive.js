c145d8474ad442871a14ebc76d98626f9dcc2c4a
Bernard Mordan <bernardmordan@gmail.com>
2014-12-27 13:53:33 +0000

    correct address for socket connection

--- public/js/drive.js
+++ public/js/drive.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://inodebot.heroku.com');
+var socket = io.connect('http://inodebot.herokuapp.com');
 var Engine = famous.core.Engine;
 var Modifier = famous.core.Modifier;
 var Transform = famous.core.Transform;
